d0e810f107b548cb40ed7195f49fde9b9831726e
adrian <adrianvisiedo2002@gmail.com>
2024-01-18 13:17:02 +0100

    retoques de ficha y casilla terminada

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,4 +1,38 @@
+//Adrián Visiedo Rodríguez
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Casilla {
+    Ficha ficha;
+
+    Casilla(){
+        ficha = null;
+    }
+
+    boolean estaOcupado(){
+        return ficha != null;
+    }
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) throws OperationNotSupportedException {
+        if (ficha==null){
+            throw new NullPointerException("No se puede poner una ficha nula.");
+        }
+        if (estaOcupado()){
+            throw new OperationNotSupportedException("La casilla ya contiene una ficha.");
+        }
+        this.ficha = ficha;
+    }
+
+    @Override
+    public String toString() {
+        String retornado= " ";
+        if (ficha != null) {
+            retornado = String.format("%s", this.ficha);
+        }
+        return retornado;
+    }
 }
--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
@@ -1,7 +1,19 @@
-//Adrián Visiedo Rodrig
+//Adrián Visiedo Rodríguez
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public enum Ficha {
-    VERDE,
-    AZUL
+    VERDE("V"),
+    AZUL("A");
+
+    private final String cadenaMostrar;
+
+    Ficha(String nombre) {
+        this.cadenaMostrar = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaMostrar;
+    }
+
 }
