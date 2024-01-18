b4a33ad64040f18c15442cee874f700208d47710
adrian <adrianvisiedo2002@gmail.com>
2024-01-18 14:35:59 +0100

    Parte del esqueleto de la clase tablero

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -0,0 +1,21 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public class Tablero {
+    static final int PUNTOS_MAXIMOS=4;
+    static final int NUMERO_FILAS=6;
+    static final int NUMERO_COLUMNAS=7;
+
+
+    public Tablero(){
+
+    }
+    public boolean estaVacio(){
+
+    }
+    private boolean columnaVacia(int columna){
+
+    }
+
+
+
+}
